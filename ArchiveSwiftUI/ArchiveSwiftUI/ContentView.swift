d549a49f4c9ddf5d41c894c3947dfe71b8c080c6
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2024-10-21 22:20:41 +0900

    앱 진입 수정

--- ArchiveSwiftUI/ArchiveSwiftUI/ContentView.swift
+++ ArchiveSwiftUI/ArchiveSwiftUI/ContentView.swift
@@ -9,7 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("ContentView")
+        NavigationStack {
+            List(
+                Row.allCases,
+                id: \.self
+            ) { row in
+                NavigationLink("\(row)") {
+                    AnyView(row.destination)
+                }
+            }
+        }
+    }
+}
+
+extension ContentView {
+    enum Row: CaseIterable, Hashable {
+        case alignment
+        case overlay
+        
+        var destination: any View {
+            switch self {
+            case .alignment:
+                AlignmentView()
+            case .overlay:
+                OverlayView()
+            }
+        }
     }
 }
 
