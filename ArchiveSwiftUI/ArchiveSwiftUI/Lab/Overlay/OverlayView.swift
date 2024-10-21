71798496339591742be30d8d7a0a2fa98a8da60d
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2024-10-21 22:58:45 +0900

    [overlay] 배경 터치하면 사라짐

--- ArchiveSwiftUI/ArchiveSwiftUI/Lab/Overlay/OverlayView.swift
+++ ArchiveSwiftUI/ArchiveSwiftUI/Lab/Overlay/OverlayView.swift
@@ -10,25 +10,44 @@ import SwiftUI
 struct OverlayView: View {
     
     @State private var viewSize: CGSize = .init()
-    @State private var view2Size: CGSize = .init()
+    @State private var isPresentOverlay: Bool = false
     
     var body: some View {
-        Text("Hello, World! ~~~ ^^\nㅎㅎ~~ㅎㅎㅎ")
-            .padding(.all, 20)
-            .background(Color.red)
-            .clipShape(RoundedRectangle(cornerRadius: 12))
-            .readSize(onChange: { size in
-                viewSize = size
-            })
-            .overlay {
+        ScrollView {
+            VStack {
+                HStack {
+                    Spacer()
+                    Text("Hello, World! ~~~ ^^\nㅎㅎ~~ㅎㅎㅎ")
+                        .padding(.all, 20)
+                        .background(Color.red)
+                        .onTapGesture {
+                            isPresentOverlay.toggle()
+                        }
+                        .clipShape(RoundedRectangle(cornerRadius: 12))
+                        .readSize(onChange: { size in
+                            viewSize = size
+                        })
+                }
+            }
+        }
+        .overlay {
+            if isPresentOverlay {
                 GeometryReader(content: { geometry in
                     Overlay2View()
                         .offset(
                             x: 0,
                             y: viewSize.height
                         )
+                        .onTapGesture {
+                            print("터치안되게하기")
+                        }
                 })
+                .background(Color.blue.opacity(0.5))
+                .onTapGesture {
+                    isPresentOverlay = false
+                }
             }
+        }
     }
 }
 
